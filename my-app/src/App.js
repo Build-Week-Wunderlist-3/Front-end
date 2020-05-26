a970a151ff072ce4da004fc9d0d4de39aa056a3b
Natalia <nataliamirandac@hotmail.com>
2020-05-26 13:23:47 -0400

    Routing ready - starting working TodoList

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+// import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import TodoList from "./components/TodoList";
+import Home from "./components/Home";
+import UpdateTodo from "./components/UpdateTodo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        Hellooo
+        <Switch>
+          {/* <Route exact path="/" component={Home} />
+          <Route path="/login" compoennt={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/updatetodo" component={UpdateTodo} />
+          <Route path="/todolist" component={TodoList} /> */}
+        </Switch>
+      </div>
+      ;
+    </Router>
   );
 }
 
